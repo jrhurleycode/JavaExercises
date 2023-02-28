6b8a6756696bba93e79e499664afc551e1a9fd6e
Josh H <jrhurley5333@gmail.com>
2023-02-27 19:50:24 -0500

    init commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("What's is your name?");
+        Scanner scanner = new Scanner(System.ini)
+
+
+    }
+}
