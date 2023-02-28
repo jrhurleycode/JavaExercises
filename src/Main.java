c378218bdb2b4f84e2f241b26361d6e3ea7417d7
Josh H <jrhurley5333@gmail.com>
2023-02-27 20:49:28 -0500

    exercises

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,24 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-        System.out.println("What's is your name?");
-        Scanner scanner = new Scanner(System.ini)
+        System.out.print("First Name: ");
+        String firstName = scanner.next();
 
+        System.out.print("Age: ");
+        int age = scanner.nextInt();
+
+        System.out.print("Amount of money in your pocket: £");
+        double money = scanner.nextDouble();
+
+        System.out.println();
+
+        double moneyNeeded = Math.ceil(money) - money;
+
+        System.out.println("Your name is " + firstName + " and you are " + age + " years old.");
+        System.out.format("You have £%.2f on you and need £%.2f.  ", money, moneyNeeded);
+        System.out.println("You've lived " + age + " years.  In another " + age + "years you'll be " + (age * 2) + "years old.");
 
     }
 }
